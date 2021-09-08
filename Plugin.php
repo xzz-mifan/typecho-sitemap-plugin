df18420db894fcaab5028d3866ac0a4e18257793
xzz <867342143@qq.com>
2021-09-08 11:56:23 +0800

    优化LastChange

--- Plugin.php
+++ Plugin.php
@@ -1,13 +1,13 @@
 <?php
 
-define('__TYPECHO_PLUGIN_SITEMAP_VERSION__', '2.0.3');
+define('__TYPECHO_PLUGIN_SITEMAP_VERSION__', '2.0.4');
 
 /**
  * <strong style="color:#28B7FF;font-family: 楷体;">生成Sitemap符合Google和百度标准</strong>
  *
  * @package    Sitemap
  * @author     <strong style="color:#28B7FF;font-family: 楷体;">lmcc</strong>
- * @version    2.0.3
+ * @version    2.0.4
  * @dependence 17.10.30-*
  * @link       https://www.lmcc.top/
  */
